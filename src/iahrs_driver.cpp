d7fd86953165a178b431e8f03fc111f4c16db62c
Wookbin Jin <58063370+wookbin@users.noreply.github.com>
2022-02-23 08:40:36 +0900

    Update iahrs_driver.cpp

--- src/iahrs_driver.cpp
+++ src/iahrs_driver.cpp
@@ -284,7 +284,7 @@ int main (int argc, char** argv)
             ros::Time measurement_time = ros::Time::now() + ros::Duration(time_offset_in_seconds);
 
 			imu_data_msg.header.stamp = measurement_time;
-			imu_data_msg.header.frame_id = "imu_link";
+			imu_data_msg.header.frame_id = tf_prefix_ + "/imu_link";  // "imu_link"
 
 			// publish the IMU data
 			imu_data_pub.publish(imu_data_msg);
@@ -294,7 +294,8 @@ int main (int argc, char** argv)
 			tf::Quaternion q;
 			q.setRPY(_pIMU_data.dEuler_angle_Roll, _pIMU_data.dEuler_angle_Pitch, _pIMU_data.dEuler_angle_Yaw);
 			transform.setRotation(q);
-			br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_link", "imu_link"));
+			//br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_link", "imu_link"));
+			br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), tf_prefix_ + "/base_link", tf_prefix_ + "/imu_link"));
 
 
 		}
